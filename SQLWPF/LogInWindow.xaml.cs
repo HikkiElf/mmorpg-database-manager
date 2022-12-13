2421c8a953dd82ed22f1990540b08855464b2517
sevaK5 <43607822+sevaK5@users.noreply.github.com>
2022-12-13 19:01:11 +0700

    add method - (validationMethod) that get allNames and allPasswords from account, add button click that show message box

--- SQLWPF/LogInWindow.xaml.cs
+++ SQLWPF/LogInWindow.xaml.cs
@@ -10,7 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
 
 namespace SQLWPF
 {
@@ -19,9 +28,73 @@ namespace SQLWPF
     /// </summary>
     public partial class LogInWindow : Window
     {
+        private List<string> allNames = new List<string>();
+        private List<string> allPasswords = new List<string>();
+
         public LogInWindow()
         {
             InitializeComponent();
+            ValidationMethod();
+        }
+
+        private void ValidationMethod()
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["mmorpgdb"].ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch(SqlException)
+                {
+                    return;
+                }
+
+                SqlCommand takeAllNames = new SqlCommand
+                {
+                    Connection = connection,
+                    CommandText = "SELECT accountName FROM Accounts"
+                };
+
+                SqlDataReader readerNames = takeAllNames.ExecuteReader();
+                while(readerNames.Read())
+                {
+                    allNames.Add((string)readerNames[0]);
+                }
+                var message = string.Join(Environment.NewLine, allNames);
+                MessageBox.Show(message);
+                readerNames.Close();
+
+                SqlCommand takeAllPasswords = new SqlCommand
+                {
+                    Connection = connection,
+                    CommandText = "SELECT accountPasswd FROM Accounts"
+                };
+
+                SqlDataReader readerPasswords = takeAllPasswords.ExecuteReader();
+                while (readerPasswords.Read())
+                {
+                    allPasswords.Add((string)readerPasswords[0]);
+                }
+                message = string.Join(Environment.NewLine, allPasswords);
+                MessageBox.Show(message);
+
+                
+
+
+            }
+            
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (allNames.Contains(AccountNameField.Text))
+            {
+                MessageBox.Show("Success");
+            }
+            else { MessageBox.Show("Wrong Account Name"); }
+
         }
     }
 }
